8de0c0b5afb48c311910e86f78d5f1330d0618b8
shradhakatyal <skatyal2204@gmail.com>
2018-03-30 17:27:54 +0530

    styling added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -15,8 +16,11 @@ class App extends Component {
 			videos: [],
 			selectedVideo: null
 		};
+		this.videoSearch('Taylor Swift');
+	}
 
-		YTSearch({key: API_KEY, term: 'Taylor Swift'}, (videos) => {
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
 			this.setState({ 
 				videos: videos,
 				selectedVideo: videos[0]
@@ -25,10 +29,12 @@ class App extends Component {
 			// It will get resolved as: this.setState({ videos: videos }); (es6 syntax)
 		});
 	}
+
 	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
 		return (
 			<div>
-				<SearchBar />
+				<SearchBar onSearchTermChange={videoSearch} />
 				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList 
 				onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
